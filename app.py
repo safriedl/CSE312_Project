effda69fc9e9f756bd5271dd35ab36c5897723bb
safriedl <safriedl@buffalo.edu>
2022-10-25 18:49:48 -0400

    Added profile pages

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
-from flask import request, Flask
+from flask import request, Flask, abort, render_template
+import psycopg2
 
 app = Flask("project name")
 
 
-
-
-
-import psycopg2
-#--------------------------------Database Code-----------------------------|
-def postgresql_system(operation, values, values2): #two functions should be allocated within another file, and called from here only.
+# --------------------------------Database Code-----------------------------|
+def postgresql_system(operation, values=None,
+                      values2=None):  # two functions should be allocated within another file, and called from here only.
     result = None
-    #Connecting to db. May need to change if setting up to connect to a local repo.
+    # Connecting to db. May need to change if setting up to connect to a local repo.
     DBhost = "DB_postgreSQL"
     database = "MathGameDB"
     username = "user345"
-    pwd = "pasword345"
+    pwd = "password345"
     port_id = 5432
 
     conn = None
     cur = None
     try:
         conn = psycopg2.connect(
-            host = DBhost,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id
-            )
+            host=DBhost,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id
+        )
         cur = conn.cursor()
 
-
         if operation == "addUsers":
-            insert_script = 'INSERT INTO users (username, password) VALUES (%s,%s)' #add username.
+            insert_script = 'INSERT INTO users (username, password) VALUES (%s,%s)'  # add username.
             cur.execute(insert_script, values)
-            
-            cur.execute('SELECT * FROM users where username = %s', (values[0],) ) #return added username.
+
+            cur.execute('SELECT * FROM users where username = %s', (values[0],))  # return added username.
             tuple = cur.fetchone()
             result = tuple
 
@@ -48,11 +45,16 @@ def postgresql_system(operation, values, values2): #two functions should be allo
             tuple = cur.fetchall()
             result = tuple
 
+        elif operation == "getUser":
+            get_script = '''SELECT * FROM users WHERE username = %s'''
+            cur.execute(get_script, values)
+            data = cur.fetchone()
+            result = data
 
-        #elif operation == "":
-        #elif operation == "":
-        #elif operation == "":
-        #else:
+        # elif operation == "":
+        # elif operation == "":
+        # elif operation == "":
+        # else:
         conn.commit()
     except Exception as error:
         print(error)
@@ -66,7 +68,7 @@ def postgresql_system(operation, values, values2): #two functions should be allo
 
 def CreateTables():
     result = None
-    #Connecting to Database. May need to change if setting up to connect to a local repo.
+    # Connecting to Database. May need to change if setting up to connect to a local repo.
     DBhost = "DB_postgreSQL"
     database = "MathGameDB"
     username = "user345"
@@ -77,15 +79,15 @@ def CreateTables():
     cur = None
     try:
         conn = psycopg2.connect(
-            host = DBhost,
-            dbname = database,
-            user = username,
-            password = pwd,
-            port = port_id
-            )
+            host=DBhost,
+            dbname=database,
+            user=username,
+            password=pwd,
+            port=port_id
+        )
         cur = conn.cursor()
 
-        #cur.execute("DROP TABLE IF EXISTS users") #will clear the table.
+        # cur.execute("DROP TABLE IF EXISTS users") #will clear the table.
         create_user = ''' CREATE TABLE IF NOT EXISTS users (
             id serial Primary Key NOT NULL,
             username varchar(40),
@@ -102,27 +104,27 @@ def CreateTables():
         if conn is not None:
             conn.close()
     return result
-#|--------------------------------Database Code-----------------------------|
-#example calls
-CreateTables()
-user = postgresql_system("addUsers", ("abc@cse312.com", "xyz"), None)
-user2 = postgresql_system("addUsers", ("abcd@cse312.com", "website"), None)
-user3 = postgresql_system("addUsers", ("abcd3@cse312.com", "website3"), None)
-user4 = postgresql_system("addUsers", ("abcd4@cse312.com", "website4"), None)
-user5 = postgresql_system("addUsers", ("abcd5@cse312.com", "website5"), None)
 
+
+# |--------------------------------Database Code-----------------------------|
+# example calls
+CreateTables()
+user = postgresql_system("addUsers", ("a", "xyz"), None)
+user2 = postgresql_system("addUsers", ("b", "website"), None)
+user3 = postgresql_system("addUsers", ("c", "website3"), None)
+user4 = postgresql_system("addUsers", ("d", "website4"), None)
+user5 = postgresql_system("addUsers", ("e", "website5"), None)
 
 LeaderBoard = postgresql_system("allUsers", None, None)
-print(LeaderBoard) #This is printed 
+print(LeaderBoard)  # This is printed
 
 
-
-
-
-
-
-
-
-@app.route('/', methods=['GET'])
-def hello():
-    return "hello"
+@app.route('/profile/<user_name>', methods=['GET'])
+def hello(user_name):
+    user_data = postgresql_system("getUser", user_name)
+    if user_data:
+        wins = 42  # leaderboard db not implemented yet, QUERY HERE
+        games_played = 420  # leaderboard db not implemented yet, QUERY HERE
+        return render_template('user_profile_template.html', wins=wins, played=games_played, user=user_name)
+    else:
+        abort(404)
--- problem.py
+++ problem.py
@@ -1,28 +1,28 @@
 import random
 
+question_set = {}
 
-class problem:
 
-    
-    question_set={}
+# pandas
+def generate_question():
+    operation_types = ["+", "-", "*", "/", "**"]
+    question_types = ["(# & (# & #))",
+                      "((# & #) & (# & #))",
+                      "((# & (# & #)) & (# & #))"]
+    question = random.choice(question_types)
+    for i in question:
+        if i == "#":
+            question = question.replace(i, str(random.randint(1, 10)), 1)
+        if i == "&":
+            question = question.replace(i, str(random.choice(operation_types)), 1)
+    ans = eval(question)
+    question_set[question] = round(ans, 4)
 
-    #pandas
-    def generate_question():
-        operation_types=["+" , "-" , "*" , "/", "**"]
-        question_types=["(# & (# & #))",
-                        "((# & #) & (# & #))",
-                        "((# & (# & #)) & (# & #))"]
-        question=random.choice(question_types)
-        for i in question:
-            if i =="#":
-                question=question.replace(i,str(random.randint(1,10)),1)
-            if i =="&":
-                question=question.replace(i,str(random.choice(operation_types)),1)
-        ans=eval(question)
-        problem.question_set[question]=round(ans,4)
-    def generator(num:int):
-        for i in range(num):
-            problem.generate_question()
 
-problem.generator(10)
-print(problem.question_set)
+def generator(num: int):
+    for i in range(num):
+        generate_question()
+
+
+generator(10)
+print(question_set)
