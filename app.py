fe76c9335c963052cf7f46885dbf7c622c5b92fa
safriedl <safriedl@buffalo.edu>
2022-11-08 18:50:01 -0500

    works now

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import request, Flask, abort, render_template, send_from_directory
+
+from flask import request, Flask, abort, render_template, redirect
+from flask_socketio import SocketIO
 
 from dbCode import *
 from problem import *
@@ -12,12 +14,17 @@ user3 = postgresql_system("addUsers", ("c", "website3"))
 user4 = postgresql_system("addUsers", ("d", "website4"))
 user5 = postgresql_system("addUsers", ("e", "website5"))
 
-app = Flask("project name")
+app = Flask("Math Duels")
+socket = SocketIO(app)
+
+lobbies = []  # list of groups of max size 2
+
+lobby_counter = 0
 
 
 @app.route('/', methods=['GET'])
-def h():
-    return "hello"
+def home():
+    return render_template("home_page.html")
 
 
 @app.route('/leaderboard', methods=['GET'])
@@ -32,13 +39,9 @@ def local_game():
     print(problemTuple)
     return render_template("local_problem.html", problem=problemTuple[0], ans=problemTuple[1])
 
-@app.route('/scripts/<script>')
-def serve_js(script):
-    return send_from_directory('scripts', script)
-
-@app.route('/profile/<user_name>', methods=['GET'])
-def hello(user_name):
-    user_data = postgresql_system("getUser", user_name)
+@app.route('/profile/<username>', methods=['GET'])
+def hello(username):
+    user_data = postgresql_system("getUser", username)
     if user_data:
         wins = user_data[1]
         games_played = user_data[4]
@@ -47,5 +50,32 @@ def hello(user_name):
         abort(404)
 
 
+@app.route('/lobby')
+def join_lobby():
+    username = ""
+    args = request.args
+    if "name" in args.keys():
+        username = args["name"]
+    users = postgresql_system("allUsers")
+    if username in users:
+        lobbies_open = sum(1 for lob in lobbies if len(lob) != 2)
+        if lobbies_open == 0:
+            lobbies[len(lobbies)] = [username]
+            return redirect(f"/lobby/{len(lobbies)}", 302)
+        else:
+            for lob in sorted(lobbies, key=lambda x: len(x), reverse=True):
+                if len(lob) < 2:
+                    if username not in lob:
+                        lob.append(username)
+                        return redirect(f"/lobby/{lobbies.index(lob)}", 302)
+    else:
+        return "You must register to join a lobby."
+
+
+@app.route('/lobby/<lobby_num>')
+def lobby(lobby_num):
+    return render_template("lobby.html", num=lobby_num, names=", ".join(lobbies[lobby_num]), async_mode=socket.async_mode)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    socket.run(app, host='0.0.0.0', port=8000, debug=True, allow_unsafe_werkzeug=True)
