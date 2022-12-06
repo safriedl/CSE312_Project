48864c6cf93cfddb3c0f4d8386abaad8d7f6f932
safriedl <safriedl@buffalo.edu>
2022-12-05 22:53:59 -0500

    simplified problem generation

--- problem.py
+++ problem.py
@@ -1,11 +1,9 @@
 import random
 
-question_set = {}
-
 
 # pandas
 def generate_question():
-    operation_types = ["+", "-", "*", "/"]
+    operation_types = ["+", "-", "*"]
     question_types = ["# & #"]
     question = random.choice(question_types)
     for i in question:
@@ -14,15 +12,4 @@ def generate_question():
         if i == "&":
             question = question.replace(i, str(random.choice(operation_types)), 1)
     ans = eval(question)
-    question_set[question] = round(ans, 4)
-    # added a return to get a question for the local problem, as a tuple of (question, answer)
-    return question, question_set[question]
-
-
-def generator(num: int):
-    for i in range(num):
-        generate_question()
-
-
-generator(10)
-print(question_set)
+    return question, round(ans, 4)
